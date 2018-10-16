8165c461226cbc8d61fa1b304d73e3b7e968a1de
Arwin Gansekoele <awgansekoele@gmail.com>
2018-10-16 14:10:26 +0200

    Finished the parser

--- loading data/parser.py
+++ loading data/parser.py
@@ -7,14 +7,14 @@ Created on Tue Oct 16 12:45:49 2018
 import argparse, os
 parser = argparse.ArgumentParser(description='Reads in experiment data for groups C and D')
 parser.add_argument('--input-path', '-i', metavar='INPUT_PATH', default=os.getcwd(),
-                        help='The folder containing the runs of the experiment')
+                        help='The folder containing the runs of the experiment. Will default to the working directory if not provided.')
 parser.add_argument('--output-path', '-o', metavar='OUTPUT_PATH', default=os.getcwd(),
-                        help='Write the solution to this filepath. In case no output is given, writes to the experiment folder')
+                        help='Write the solution to this filepath. In case no output is given, writes to the experiment folder.')
 parser.add_argument('--experiment-name', '-e', metavar = 'EXPERIMENT_NAME', required=True,
-                        help='The seed used for the random number generator')
+                        help='The name of the experiment that was performed.')
 parser.add_argument('--world-name', '-w', metavar = 'WORLD_NAME', required=True,
-                        help='The name of the world used for this experiment')
+                        help='The name of the world used for this experiment. ')
 parser.add_argument('--group-name', '-g', metavar = 'GROUP_NAME', required=True,
-                        help='The name of the group who ran the experiment e.g. C, D or CD')
+                        help='The name of the group who ran the experiment e.g. C, D or CD.')
 args = parser.parse_args()
 

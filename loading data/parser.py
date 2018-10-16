20db501db3f9c875149abcf575bebcdd54911154
Arwin Gansekoele <awgansekoele@gmail.com>
2018-10-16 14:05:53 +0200

    Added the argparse procedure

---
+++ loading data/parser.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 16 12:45:49 2018
+
+@author: Arwin
+"""
+import argparse, os
+parser = argparse.ArgumentParser(description='Reads in experiment data for groups C and D')
+parser.add_argument('--input-path', '-i', metavar='INPUT_PATH', default=os.getcwd(),
+                        help='The folder containing the runs of the experiment')
+parser.add_argument('--output-path', '-o', metavar='OUTPUT_PATH', default=os.getcwd(),
+                        help='Write the solution to this filepath. In case no output is given, writes to the experiment folder')
+parser.add_argument('--experiment-name', '-e', metavar = 'EXPERIMENT_NAME', required=True,
+                        help='The seed used for the random number generator')
+parser.add_argument('--world-name', '-w', metavar = 'WORLD_NAME', required=True,
+                        help='The name of the world used for this experiment')
+parser.add_argument('--group-name', '-g', metavar = 'GROUP_NAME', required=True,
+                        help='The name of the group who ran the experiment e.g. C, D or CD')
+args = parser.parse_args()
+
